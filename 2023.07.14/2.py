f59ff41aec3891110d574a9585847837a374d403
Dimon2kg <138508655+Dimon2kg@users.noreply.github.com>
2023-07-25 22:23:10 +0500

    Update 2.py

--- 2023.07.14/2.py
+++ 2023.07.14/2.py
@@ -1,1 +1,9 @@
-
+a = int(input())
+b = int(input())
+if a%b == 0:
+    print("%d делится на %d нацело" % (a,b))
+    print("частное: %d" % (a//b))
+else:
+    print("%d не делится на %d" % (a,b))
+    print("неполное частное: %d" % (a//b))
+    print("остаток: %d" % (a%b))
